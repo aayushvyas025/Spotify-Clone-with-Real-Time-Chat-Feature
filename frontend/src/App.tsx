d31813cabb7ea98b55501d3c2fde8953ddf99035
Aayush Vyas <aayush25vyas@gmail.com>
2025-11-27 23:28:54 +0530

    Setup the basic Frontend Folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+    <h1>Hello World</h1>
+    <Button variant={"secondary"}>Shadcn Button</Button>
+    </>
+  )
+}
+
+export default App
