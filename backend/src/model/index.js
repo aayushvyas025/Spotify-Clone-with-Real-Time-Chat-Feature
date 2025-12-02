0799cc97db7f9bb9936e9057c180e8ad75e17180
Aayush Vyas <aayush25vyas@gmail.com>
2025-12-02 16:31:39 +0530

    Created the Message Model for Database

--- backend/src/model/index.js
+++ backend/src/model/index.js
@@ -1,3 +1,4 @@
 export {default as User} from "./users/user.model.js"; 
 export {default as Song} from "./songs/song.model.js";
-export {default as Album} from "./albums/album.model.js"
+export {default as Album} from "./albums/album.model.js";
+export {default as Message} from "./messages/message.model.js"
