9cdec6e554c58ae8933ab76bc77978eccde3eaef
Aayush Vyas <aayush25vyas@gmail.com>
2025-12-22 09:05:43 +0530

    Using Zustand for Global State Management

---
+++ frontend/src/store/music/musicStore.ts
@@ -0,0 +1,16 @@
+import {create} from "zustand"
+
+
+const useMusicStore = create((set) => ({
+ albums:[],
+ songs:[],
+ fetchAlbums: async()=> {
+    try {
+        
+    } catch (error:any) {
+        
+    }
+ }
+})); 
+
+export default useMusicStore; 
