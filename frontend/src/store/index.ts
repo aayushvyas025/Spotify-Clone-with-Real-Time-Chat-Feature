b68907fa049bc04085cc8739480d53fc68531d96
Aayush Vyas <aayush25vyas@gmail.com>
2026-01-04 17:45:28 +0530

    Create the AudioPlayer component for playing music

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,3 +1,5 @@
 export {default as useMusicStore} from "./music/musicStore";
 export {default as useChatStore} from "./chat/chatStore";
-export {default as useAuthStore} from "./auth/authStore"
+export {default as useAuthStore} from "./auth/authStore";
+export {default as usePlayerStore} from "./audio/playerStore"
+
