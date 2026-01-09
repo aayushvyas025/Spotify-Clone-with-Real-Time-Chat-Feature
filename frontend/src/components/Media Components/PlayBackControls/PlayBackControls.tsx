7cb138a8978208c757b8390ec96db10eaf7eba74
Aayush Vyas <aayush25vyas@gmail.com>
2026-01-09 16:33:22 +0530

    Fixed the Overflow issue in PlayBackControls Component

--- frontend/src/components/Media Components/PlayBackControls/PlayBackControls.tsx
+++ frontend/src/components/Media Components/PlayBackControls/PlayBackControls.tsx
@@ -1,6 +1,5 @@
 import { PlayBackSongInfo } from "@/components/Song Components";
-import { MediaPlayer, MediaPlayerBar, MediaPlayerOptions } from "@/components/Media Components";
-import { usePlayBackControls } from "@/hooks";
+import { MediaPlayer, MediaPlayerOptions } from "@/components/Media Components";
 import { helperFunctions } from "@/helper";
 
 const {songDurationFormatter} = helperFunctions;
@@ -9,7 +8,7 @@ function PlayBackControls() {
  
  return (
     <footer className="h-20 sm:h-20 bg-zinc-900 border-t border-zinc-800 px-4">
-      <div className="flex justify-between items-center h-full max-w-[1080px] mx-auto">
+      <div className="flex justify-between items-center h-full w-full mx-auto">
         <PlayBackSongInfo />
         <MediaPlayer  />
         
