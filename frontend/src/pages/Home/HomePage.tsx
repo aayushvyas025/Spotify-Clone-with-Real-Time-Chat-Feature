aca336d9bb1ccfc19100bde31232ae8883c87e5c
Aayush Vyas <aayush25vyas@gmail.com>
2026-01-09 18:29:37 +0530

    Implement Song queue in Home Page

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -1,22 +1,42 @@
 import { HomeContentContainer } from "@/components/Home Components";
-import { useMusicStore } from "@/store";
+import { useMusicStore, usePlayerStore } from "@/store";
 import { useUser } from "@clerk/clerk-react";
 import { useEffect } from "react";
 
 function HomePage() {
-  const { fetchFeaturedSongs, fetchTrendingSongs, fetchForUserSongs } =
-    useMusicStore();
+  const {
+    fetchFeaturedSongs,
+    fetchTrendingSongs,
+    fetchForUserSongs,
+    madeForYouSongs,
+    featuredSongs,
+    trendingSongs,
+  } = useMusicStore();
+  const { initialQueue } = usePlayerStore();
   const { user } = useUser();
 
-  useEffect(() => {  
+  useEffect(() => {
     fetchFeaturedSongs();
     fetchTrendingSongs();
     fetchForUserSongs();
   }, [user, fetchFeaturedSongs, fetchForUserSongs, fetchTrendingSongs]);
 
+  useEffect(() => {
+    if (!madeForYouSongs || !featuredSongs || !trendingSongs) return;
+
+    if (
+      madeForYouSongs.length > 0 &&
+      featuredSongs.length > 0 &&
+      trendingSongs.length > 0
+    ) {
+      const allSongs = [...madeForYouSongs, ...featuredSongs, ...trendingSongs];
+      initialQueue(allSongs);
+    }
+  }, [initialQueue, madeForYouSongs, featuredSongs, trendingSongs]);
+
   return (
     <main className="overflow-hidden rounded-md h-full bg-gradient-to-b from-zinc-800 to-zinc-900">
-       <HomeContentContainer />
+      <HomeContentContainer />
     </main>
   );
 }
