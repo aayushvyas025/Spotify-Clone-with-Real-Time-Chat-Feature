da59dea32f08558edcc52c56b474b3a3f993b2a5
Aayush Vyas <aayush25vyas@gmail.com>
2026-01-09 18:12:48 +0530

    Using Dynamic Button in MediaPlayer Instead of Individual Buttons of Repeat and Shuffle

--- frontend/src/components/Button Components/index.ts
+++ frontend/src/components/Button Components/index.ts
@@ -3,9 +3,7 @@ export {default as SigninButton} from "./SigninButton/SigninButton";
 export {default as SignInOAuthButton} from "./SignInOAuthButtons/SignInOAuthButton";
 export {default as AlbumSongPlayButton} from "./AlbumSongPlayButton/AlbumSongPlayButton";
 export {default as ReusableSongPlayButton} from "./ReusableSongPlayButton/ReusableSongPlayButton";
-export {default as ShuffleButton} from "./ShuffleButton/ShuffleButton";
 export {default as PlayPreviousButton} from "./PlayPreviousButton/PlayPreviousButton";
 export {default as MediaPlayerToggleBtn} from "./MediaPlayerToggleBtn/MediaPlayerToggleBtn"; 
-export {default as RepeatButton} from "./RepeatButton/RepeatButton";
 export {default as PlayNextButton} from "./PlayNextButton/PlayNextButton";
 export {default as DynamicButton} from "./DynamicButton/DynamicButton"
--- frontend/src/components/Media Components/MediaPlayerControls/MediaPlayerControls.tsx
+++ frontend/src/components/Media Components/MediaPlayerControls/MediaPlayerControls.tsx
@@ -1,25 +1,41 @@
-import { MediaPlayerToggleBtn, PlayPreviousButton, RepeatButton, ShuffleButton, PlayNextButton } from '@/components/Button Components'
-import { usePlayBackControls } from '@/hooks';
-
+import {
+  MediaPlayerToggleBtn,
+  PlayPreviousButton,
+  PlayNextButton,
+  DynamicButton,
+} from "@/components/Button Components";
+import { usePlayBackControls } from "@/hooks";
+import { Repeat, Shuffle } from "lucide-react";
 
 function MediaPlayerControls() {
-    const { currentSong, playPrevious, isPlaying, togglePlay, playNext} = usePlayBackControls();
+  const { currentSong, playPrevious, isPlaying, togglePlay, playNext } =
+    usePlayBackControls();
   return (
-      <div className="flex items-center justify-center gap-4 sm:gap-6">
-        <ShuffleButton />
-        <PlayPreviousButton
-          disable={!currentSong}
-          onClickHandler={playPrevious}
-        />
-        <MediaPlayerToggleBtn
-          disable={!currentSong}
-          musicPlaying={isPlaying}
-          onClickHandler={togglePlay}
-        />
-        <PlayNextButton disable={!currentSong} onClickHandler={playNext} />
-        <RepeatButton />
-      </div>
-  )
+    <div className="flex items-center justify-center gap-4 sm:gap-6">
+      <DynamicButton
+        size={"icon"}
+        btnVariant={"ghost"}
+        styles="hidden sm:inline-flex hover:text-white text-zinc-400"
+        btnIcon={<Shuffle className="h-4 w-4" />}
+      />
+      <PlayPreviousButton
+        disable={!currentSong}
+        onClickHandler={playPrevious}
+      />
+      <MediaPlayerToggleBtn
+        disable={!currentSong}
+        musicPlaying={isPlaying}
+        onClickHandler={togglePlay}
+      />
+      <PlayNextButton disable={!currentSong} onClickHandler={playNext} />
+       <DynamicButton
+        size={"icon"}
+        btnVariant={"ghost"}
+        styles="hidden sm:inline-flex hover:text-white text-zinc-400"
+        btnIcon={<Repeat className="h-4 w-4" />}
+      />
+    </div>
+  );
 }
 
-export default MediaPlayerControls
+export default MediaPlayerControls;
